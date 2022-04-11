4ee957da997f70897b05bc079569ad227586d840
Samu <samu.landen@gmail.com>
2022-04-11 22:28:41 +0300

    added menu

--- js/main.js
+++ js/main.js
@@ -9,20 +9,42 @@ function getDrink(){
     .then(res => res.json())
     .then(data => {
 
+        let listOfDrinks = data.drinks.map(a => a.strDrink)
+        
+
+        let list = document.getElementById("drinkList");
+
+        list.innerHTML = ''
+
+        listOfDrinks.forEach((item) => {
+            let li = document.createElement("li");
+            li.innerText = item;
+            list.appendChild(li);
+         });
+
+        document.querySelector('ul').style.background = 'white'
+
+        
+
+        
+
         let ingredients = [data.drinks[0].strIngredient1,data.drinks[0].strIngredient2,data.drinks[0].strIngredient3,data.drinks[0].strIngredient4,data.drinks[0].strIngredient5,data.drinks[0].strIngredient6,data.drinks[0].strIngredient7,data.drinks[0].strIngredient8,data.drinks[0].strIngredient9,data.drinks[0].strIngredient10,data.drinks[0].strIngredient11,data.drinks[0].strIngredient12,data.drinks[0].strIngredient13,data.drinks[0].strIngredient14,data.drinks[0].strIngredient15]
 
         let actualIngredients = []
 
         ingredients.forEach (x => {if(x !== null){actualIngredients.push(x)};})
 
-        console.log(actualIngredients);
+        // console.log(actualIngredients);
 
         document.querySelector('h2').innerText = data.drinks[0].strDrink
         document.querySelector('img').src = data.drinks[0].strDrinkThumb
         document.querySelector('img').style.padding = '10px'
         document.querySelector('#ingredient').innerText = actualIngredients.join(', ')
         document.querySelector('#instructions').innerText = data.drinks[0].strInstructions
-        console.log(data.drinks);
+        // console.log(data);
+
+        // console.log(listOfDrinks);
+        
     })
     .catch(err => {
         console.log(`error ${err}`)
@@ -32,3 +54,13 @@ function getDrink(){
 
 }
 
+document.querySelector('ul').addEventListener('click',random)
+
+function random(evt){
+
+    
+    
+
+   alert(evt.target.innerText)
+}
+    
